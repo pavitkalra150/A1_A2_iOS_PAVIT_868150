fcab908643258f009b5676c6e7d7574d88156a9f
pavitkalra150 <c0868150@mylambton.ca>
2023-01-20 16:55:48 -0500

    Initial Commit

---
+++ A1_A2_iOS_ PAVIT_868150/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  A1_A2_iOS_ PAVIT_868150
+//
+//  Created by PAVIT KALRA on 2023-01-20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
